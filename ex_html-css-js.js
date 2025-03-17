7ce5c85e467fac5c4717f2060cc24ad9343bdd33
jdelsan275 <jdelsan275@g.educaand.es>
2025-03-17 18:13:34 +0100

    .

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,43 @@
 //Escribe aquí tu código JavaScript
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("Formulario_hamburguesa");
+    const span = document.getElementById("totalpedido");
+    const instrucciones = document.getElementById("instrucciones");
+    const contador = document.getElementById("contador");
+
+    instrucciones.oninput = () => {
+        contador.textContent = `${instrucciones.value.length}/100 caracteres`;
+    };
+
+    const totalPedido = () => {
+        let precio = Number(document.getElementById("tipo").value);
+        let extras = document.querySelectorAll(".extras:checked").length;
+        if (extras<2) extras = 2;
+        let cantidad = Number(document.getElementById("cantidad").value);
+        let total = (precio+extras) * cantidad;
+        span.textContent = `${total} €`;
+        return total;
+    };
+
+    document.querySelectorAll("input, select").forEach(el => {
+        el.onchange = totalPedido;
+    });
+
+    form.onsubmit = (e) => {
+        const nombre = document.getElementById("nombre").value.trim();
+        const apellidos = document.getElementById("apellidos").value.trim();
+        const direccion = document.getElementById("direccion").value.trim();
+        const telefono = document.getElementById("telefono").value.trim();
+        const email = document.getElementById("email").value.trim();
+
+        if(nombre === "" || apellidos === "" || direccion.length < 15 || !/^\d{9,}$/.test(telefono) || !email.includes("@")) {
+            alert("Error, hay campos obligatorios");
+            e.preventDefault();
+        } else if(confirm("¿Enviar pedido?")) {
+            alert(`Gracias por tu pedido.`);
+        } else {
+            e.preventDefault();
+        }
+    };
+    totalPedido();
+});
